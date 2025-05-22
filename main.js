4565727b2ac5361d7986f962095ff7ac356f57a5
prateep <prateepmoon@gmail.com>
2025-05-22 17:43:43 +0530

    news commit

---
+++ main.js
@@ -0,0 +1,64 @@
+const API_KEY = "6793577008c141629eafc9a7d1b1d553";
+const url = "https://newsapi.org/v2/everything?q=";
+
+window.addEventListener(`load`, () => fetchNews("india"));
+
+async function fetchNews(query) {
+  const res = await fetch(`${url}${query}&apikey=${API_KEY}`);
+  const data = await res.json();
+  findData(data.articles);
+}
+
+function findData(articles) {
+  const cardsContainer = document.getElementById(`cards-container`);
+  const newsCardTemplate = document.getElementById(`template-news-card`);
+
+  cardsContainer.innerHTML = "";
+
+  articles.forEach((articles) => {
+    if (!articles.urlToImage) return;
+    const cardClone = newsCardTemplate.content.cloneNode(true);
+    fillDataInCard(cardClone, articles);
+    cardsContainer.appendChild(cardClone);
+  });
+}
+
+function fillDataInCard(cardClone, articles) {
+  const newsImg = cardClone.querySelector(`#news-img`);
+  const newsTitle = cardClone.querySelector(`#news-title`);
+  const newsSource = cardClone.querySelector(`#news-source`);
+  const newsDesc = cardClone.querySelector(`#news-desc`);
+
+  newsImg.src = articles.urlToImage;
+  newsTitle.innerHTML = articles.title;
+  newsDesc.innerHTML = articles.description;
+
+  const date = new Date(articles.publishedAt).toLocaleString("en-US", {
+    timezone: "Asia/Jakarta",
+  });
+
+  newsSource.innerHTML = `${articles.source.name} • ${date}`;
+
+  cardClone.firstElementChild.addEventListener("click", () => {
+    window.open(articles.url, "_blank");
+  });
+}
+
+let curSelectedNav = null;
+function onNavItemClick(id) {
+  fetchNews(id);
+  const navItem = document.getElementById(id);
+  curSelectedNav = navItem;
+  curSelectedNav.classList.add(`active`);
+}
+
+const searchButton = document.getElementById(`search-button`);
+
+const searchText = document.getElementById(`search-text`);
+
+searchButton.addEventListener("chick", () => {
+  const query = searchText.value;
+  if (!query) return;
+  fetchNews(query);
+  curSelectedNav?.classList.remove("active");
+});
